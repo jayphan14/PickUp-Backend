eae2dbe8ef30573ee42d5fb4a3924c1b0c671ff2
jayphan14 <kha21052004@gmail.com>
2023-08-26 12:21:03 -0400

    init project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+// MIDDLEWARE
+app.use(cors({
+    credentials: true,
+}))
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+})
